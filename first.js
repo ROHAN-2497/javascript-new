fbdc5ae102b2b775187e2af1ddf1a2f1efce2022
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-14 07:28:51 +0600

    DOM Manipulate revise

---
+++ first.js
@@ -0,0 +1,7 @@
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+// console.log(heading);
+
+let headings = document.getElementsByClassName("heading-class");
+console.log(headings);
+console.dir(headings);
