ba186f6aa7aed56d3c87e37e3b9a0c6cc4ac1bc4
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-15 07:07:36 +0600

    k

--- first.js
+++
@@ -1,7 +0,0 @@
-// let heading = document.getElementById("heading");
-// console.dir(heading);
-// console.log(heading);
-
-let headings = document.getElementsByClassName("heading-class");
-console.log(headings);
-console.dir(headings);
