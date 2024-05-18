f3b5a62c70ab72e36a824b6c7d319fcdd76de78a
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-18 23:29:45 +0600

    jkl

--- first.js
+++ first.js
@@ -1,4 +1,10 @@
 let btn = document.createElement("button");
 btn.innerText = "Click me!";
+btn.style.color = "white";
 btn.style.backgroundColor = "red";
-btn.querySelector("");
+document.querySelector("body").prepend(btn);
+
+// Qs2
+
+let parar = document.querySelector("p");
+parar.classList.add("para", "newClass");
