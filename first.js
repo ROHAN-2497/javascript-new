6d6320f730557ef9c486981bce242b022d333393
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-18 21:39:59 +0600

    k

--- first.js
+++ first.js
@@ -1,7 +1,4 @@
-// let newHeading = document.createElement("h1");
-// newHeading.innerHTML = "<i>Hi, iam a Developer</i>";
-// document.querySelector("body").prepend(newHeading);
-
-let comeFriend = document.createElement("p");
-comeFriend.innerText = "2nd title";
-document.querySelector("body").append(comeFriend);
+let btn = document.createElement("button");
+btn.innerText = "Click me!";
+btn.style.backgroundColor = "red";
+btn.querySelector("");
