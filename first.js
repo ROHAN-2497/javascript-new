c95880ef1546fc8bb7e539bcab85d9cf8c9d2700
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-20 02:25:21 +0600

    p[

--- first.js
+++ first.js
@@ -1,8 +1,12 @@
 let btn1 = document.querySelector("#btn1");
-
-let btn1.onC = () => {
-  console.log("btn clicked");
-  let a = 24;
+btn1.onclick = () => {
+  console.log("object");
+  let a = 25;
   a++;
   console.log(a);
+};
+
+let div = document.querySelector("div");
+div.onmouseover = () => {
+  console.log("object");
 };
